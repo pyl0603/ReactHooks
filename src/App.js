a4fed6b02bc59e3cf476ad0eb351b6e90af5f14b
pyl0603 <742531312@qq.com>
2022-01-07 18:03:38 +0800

    21321

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/index.js'
 import AddInput from './components/AddInput/index.js';
+import React , { useState } from 'react'
 function App() {
+  const [ isInputShow,SetIsInputShow ] = useState(false)
   return (
     <div className="App">
-      <Header/>
-      <AddInput isInputShow={true}/>
+      <Header openInput={()=> SetIsInputShow(!isInputShow)}/>
+      <AddInput isInputShow={isInputShow}/>
     </div>
   );
 }
